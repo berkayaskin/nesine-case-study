139c6fa44d696828cc3e7db0043253fd53022578
berkayaskin <berkayaskiin@gmail.com>
2023-08-14 14:32:21 +0300

    feat(components/bulletin): make specified table cells selectable
    create functionality to add and remove selected cells to/from coupon context state

--- src/components/bulletin/index.tsx
+++ src/components/bulletin/index.tsx
@@ -12,13 +12,27 @@ import { fetchSize } from '@/data/constants'
 import { getBetsQueryFn } from '@/lib/api/getBetsQueryFn'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import {
+  ColumnDef,
+  Row,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 import { useVirtual } from '@tanstack/react-virtual'
-import { Fragment, memo, useCallback, useEffect, useMemo, useRef } from 'react'
-import { columns } from './columns'
+import {
+  Fragment,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+
+import { Coupon, useCoupon } from '@/contexts/coupon-context'
+import { cn } from '@/lib/utils'
+import { Button } from '../ui/button'
+import { TransformedBet } from './transformed-bets.types'
 
 const Bulletin = () => {
   /*
@@ -28,6 +42,331 @@ const Bulletin = () => {
    */
 
   const tableContainerRef = useRef<HTMLDivElement>(null)
+  const [selectedRowId, setSelectedRowId] = useState<string | null>(null)
+
+  const { coupon, setCoupon } = useCoupon()
+
+  const handleCellClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    cellValue: string,
+    row: Row<TransformedBet>
+  ) => {
+    e.preventDefault()
+
+    const rowId = row.original.id
+    const isCellSelected = isSelected(rowId, cellValue)
+
+    if (isCellSelected) {
+      // If the cell is already selected, remove it from the coupon array
+      setCoupon((prev) => {
+        if (prev) {
+          return prev.filter(
+            (c) => !(c.id === rowId && c.oddValue === cellValue)
+          )
+        }
+        return [] as Coupon[]
+      })
+    } else {
+      // If the cell is not selected, add or update it in the coupon array
+      setCoupon((prev) => {
+        if (prev) {
+          return prev
+            .filter((c) => c.id !== rowId)
+            .concat({
+              id: rowId,
+              name: row.original.name,
+              oddValue: cellValue,
+              minBetStake: row.original.oddCategories[0].minBetStake,
+            })
+        }
+        // If there's no previous coupon state, just return the new item
+        return [
+          {
+            id: rowId,
+            name: row.original.name,
+            oddValue: cellValue,
+            minBetStake: row.original.oddCategories[0].minBetStake,
+          },
+        ]
+      })
+    }
+
+    setSelectedRowId(rowId)
+  }
+
+  const isSelected = (rowId: string, cellValue: string) => {
+    return coupon?.some((c) => c.id === rowId && c.oddValue === cellValue)
+  }
+
+  const columns: ColumnDef<TransformedBet>[] = [
+    {
+      header: (props) => (
+        <span>Event Count: {props.table.getRowModel().rows.length}</span>
+      ),
+      id: 'eventCount',
+      cell: ({ row }) => {
+        return (
+          <p className="min-w-max space-x-1 text-left">
+            <span className="font-bold">{row.original.id}</span>
+            <span>{row.original.time}</span>
+            <span>{row.original.name}</span>
+          </p>
+        )
+      },
+    },
+    {
+      accessorKey: 'comments',
+      header: 'Yorumlar',
+      cell: ({ row }) => {
+        return <p>{row.original.comments}</p>
+      },
+    },
+    {
+      accessorFn: (row) => row.oddCategories[0].minBetStake,
+      id: 'minBetStake',
+      header: '',
+    },
+    {
+      accessorFn: (row) => row.oddCategories[0].odds[0].value,
+      id: 'oddCategoryName-1',
+      header: '1',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[0].odds[0].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      accessorFn: (row) => row.oddCategories[0].odds[1].value,
+      id: 'oddCategoryName-x',
+      header: 'x',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[0].odds[1].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      header: '2',
+    },
+    {
+      accessorFn: (row) => row.oddCategories[2].odds[0].value,
+      id: 'oddCategoryName-alt',
+      header: 'Alt',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[2].odds[0].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      accessorFn: (row) => row.oddCategories[2].odds[1].value,
+      id: 'oddCategoryName-ust',
+      header: 'Üst',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[2].odds[1].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      id: 'handicap-home',
+      header: 'H1',
+    },
+    {
+      id: 'handicap-1',
+      header: '1',
+    },
+    {
+      id: 'handicap-x',
+      header: 'x',
+    },
+    {
+      id: 'handicap-2',
+      header: '2',
+    },
+    {
+      id: 'handicap-away',
+      header: 'H2',
+    },
+    {
+      accessorFn: (row) => row.oddCategories[1].odds[0].value,
+      id: 'oddCategoryName-1x',
+      header: '1-X',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[1].odds[0].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      accessorFn: (row) => row.oddCategories[1].odds[1].value,
+      id: 'oddCategoryName-12',
+      header: '1-2',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[1].odds[1].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn(
+              'text-blue-500 underline hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500',
+              {
+                'bg-yellow-200 no-underline hover:bg-yellow-200':
+                  (isRowSelected && isSelected(rowId, oddValue)) ||
+                  isCouponSelected,
+              }
+            )}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      accessorFn: (row) => row.oddCategories[1].odds[2].value,
+      id: 'oddCategoryName-x2',
+      header: 'X-2',
+      cell: ({ row }) => {
+        const oddValue = row.original.oddCategories[1].odds[2].value
+        const isCouponSelected = coupon?.some(
+          (c) => c.id === row.original.id && c.oddValue === oddValue
+        )
+        const rowId = row.original.id
+        const isRowSelected = selectedRowId === rowId
+
+        return (
+          <Button
+            variant="ghost"
+            onClick={(e) => {
+              handleCellClick(e, oddValue, row)
+            }}
+            className={cn('text-blue-500 underline hover:text-blue-600', {
+              'bg-yellow-200 no-underline hover:bg-yellow-200':
+                (isRowSelected && isSelected(rowId, oddValue)) ||
+                isCouponSelected,
+            })}
+          >
+            {oddValue}
+          </Button>
+        )
+      },
+    },
+    {
+      header: 'Var',
+    },
+    {
+      header: 'Yok',
+    },
+    {
+      accessorFn: (row) => row.moreOddOptions.value,
+      id: 'moreOddOptions',
+      header: '+99',
+    },
+  ]
 
   const { data, fetchNextPage, isFetching, isLoading } = useInfiniteQuery(
     ['table-data'],
